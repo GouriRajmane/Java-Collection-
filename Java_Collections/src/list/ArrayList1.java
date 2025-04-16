54faba1c5f2a278f5c9306d53b5bd16f0aa673b0
Gouri Rajmane <rajmanegouri@gmail.com>
2025-04-16 12:30:10 +0530

    initial commit with ArrayList1.java

---
+++ Java_Collections/src/list/ArrayList1.java
@@ -0,0 +1,16 @@
+package list;
+
+import java.util.*;
+
+class ArrayList1{
+public static void main(String [] ar){
+ArrayList<String> list = new ArrayList<String>();//creating ArrayList
+list.add("Ravi"); // Addding object in arraylist
+list.add("Ajay"); 
+list.add("Amit"); 
+list.add("Vijay");
+
+//invoking arraylist objects
+System.out.println(list);
+}
+}
